f55f4fb81fba09e3e0c0b71208ff51e035c41970
thoughtsunificator <85041649+thoughtsunificator@users.noreply.github.com>
2021-09-24 01:05:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# domodel-diary-extension
+
+Browser extension context for [domodel-diary](https://github.com/thoughtsunificator/domodel-diary)
+
+![](screenshot.png)
+
+- Calendar
+- Daily notes encrypted using AES
+- Password Protected
+
+Available on:
+
+- [Firefox](https://addons.mozilla.org/en-US/firefox/addon/domodel-diary/)
+- [Chrome](https://chrome.google.com/webstore/detail/domodel-diary/hncoaagegcdnajffjpkldhfceipfgnnf)
+
+## Getting started
+
+### Installing
+
+### Run
+
+- ``npm install``
+- ``npm start``
+
+### Build
+
+- ``npm install``
+- ``npm run build``
---
+++ rollup.config.prod.js
@@ -0,0 +1,59 @@
+import path from "path"
+import del from "rollup-plugin-delete"
+import postcss from "rollup-plugin-postcss"
+import postcssImport from "postcss-import"
+import { terser } from "rollup-plugin-terser"
+import copy from "rollup-plugin-copy-watch"
+import { nodeResolve } from "@rollup/plugin-node-resolve"
+import rootImport from "rollup-plugin-root-import"
+import alias from "@rollup/plugin-alias"
+import webExt from 'web-ext';
+import { babel } from '@rollup/plugin-babel';
+
+export default {
+	input: "./src/main.js",
+	output: {
+			file: "./dist/prod/public/bundle.js",
+			format: "iife",
+	},
+	plugins: [
+		del({ targets: "dist/prod" }),
+		postcss({
+			extract: true,
+			minimize: true,
+			extract: path.resolve("dist/prod/public/bundle.css"),
+			plugins: [postcssImport()]
+		}),
+		copy({
+			targets: [{ src: "public", dest: "dist/prod" }, { src: "LICENSE", dest: "dist/prod/public" }, { src: "README.md", dest: "dist/prod/public" }],
+			flatten: false
+		}),
+		nodeResolve(),
+		alias({
+			entries: [
+				{ find:/^lib\/(.*)/, replacement: "./lib/$1" },
+				{ find:/^assets\/(.*)/, replacement: "./assets/$1" },
+				{ find:/^data\/(.*)/, replacement: "./data/$1" },
+				{ find:/^\.env\.js$/, replacement: "./.env.js" }
+			]
+		}),
+		rootImport({
+			root: `${__dirname}/src`,
+			useInput: "prepend",
+			extensions: ".js",
+		}),
+		terser(),
+		babel({ babelHelpers: 'bundled' }),
+		{
+			name: 'web-ext',
+			generateBundle() {
+				webExt.cmd.build({
+					sourceDir: path.resolve("dist/prod/public/"),
+					filename: "domodel-diary.zip",
+					artifactsDir: "build",
+					overwriteDest: true
+				}, {}).then((extensionRunner) => {});
+			}
+		}
+	]
+}
